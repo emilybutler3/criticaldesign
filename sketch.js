317257d91339c3626c2f112c890726add9226157
Emily Butler <ekb21a@fsu.edu>
2023-10-10 08:55:58 -0400

    commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let img;
+let input;
+
+function preload(){
+  img = loadImage("assets/pic.jpg");
+}
 function setup() {
   createCanvas(400, 400);
+  image(img, 0, 0);
+  
+  slider = createSlider(10, 100, 86);
+  input = createInput('');
 }
 
 function draw() {
-  background(125);
+  background(120);
+  image(img, 0, 0);
+
 }
