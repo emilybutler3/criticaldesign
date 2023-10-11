6b4bf0ee21b5eb06ac4e8e86b68318b43c86a120
Emily Butler <ekb21a@fsu.edu>
2023-10-11 01:51:13 -0400

    second commit

--- sketch.js
+++ sketch.js
@@ -1,19 +1,61 @@
 let img;
 let input;
+let button;
+let timer = 0;
+let keepShowingImage = false;
+let state = 1;
+let myURLs = ["first","second","third"];
 
-function preload(){
-  img = loadImage("assets/pic.jpg");
-}
-function setup() {
-  createCanvas(400, 400);
-  image(img, 0, 0);
+
+// function preload() {
   
-  slider = createSlider(10, 100, 86);
+// }
+
+function setup() {
+  noCanvas();
+
+  
+  img = createImg("https://i.pinimg.com/474x/6e/36/04/6e360403267c3137b97be396832e61d4.jpg");
   input = createInput('');
+  button = createButton("Submit");
+  button.mouseClicked(buttonClicked);
+}
+
+function buttonClicked() {
+  img.show();
+  keepShowingImage = true;
+  // state = 1;
 }
 
 function draw() {
-  background(120);
-  image(img, 0, 0);
 
+
+timer++;
+  if (timer > 30 && keepShowingImage == false)   {
+    img.hide();
+  }
+
+  // switch (state) {
+  //   case 1:
+  //     img.show();
+  //     button.hide();
+  //     input.hide();
+  //     break;
+  //   case 2:
+  //     img.hide();
+  //     button.show();
+  //     input.show();
+  //     break;
+  //   case 3:
+  //     img.show();
+  //     button.hide();
+  //     input.hide();
+  //     break;
+  // }
+
+  // if (state == 1 && timer > 30) {
+  //   state = 2;
+  // }
+
+  
 }
